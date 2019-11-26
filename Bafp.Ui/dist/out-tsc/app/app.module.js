e6562cbd7626cd0b65a20f1c3dd68f1d84640729
dmytro.m <dmytro.m@sbtech.com>
2019-11-26 13:44:10 +0200

    add UI project (just framework)

---
+++ Bafp.Ui/dist/out-tsc/app/app.module.js
@@ -0,0 +1,42 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        NgModule({
+            imports: [
+                BrowserModule,
+                ReactiveFormsModule,
+                RouterModule.forRoot([
+                    { path: '', component: ProductListComponent },
+                ])
+            ],
+            declarations: [
+                AppComponent,
+                TopBarComponent,
+                ProductListComponent
+            ],
+            bootstrap: [AppComponent]
+        })
+    ], AppModule);
+    return AppModule;
+}());
+export { AppModule };
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/ 
+//# sourceMappingURL=app.module.js.map
---
+++ Bafp.Ui/dist/out-tsc/app/product-list/product-list.component.js
@@ -0,0 +1,31 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+import { Component } from '@angular/core';
+import { products } from '../products';
+var ProductListComponent = /** @class */ (function () {
+    function ProductListComponent() {
+        this.products = products;
+    }
+    ProductListComponent.prototype.share = function () {
+        window.alert('The product has been shared!');
+    };
+    ProductListComponent = __decorate([
+        Component({
+            selector: 'app-product-list',
+            templateUrl: './product-list.component.html',
+            styleUrls: ['./product-list.component.css']
+        })
+    ], ProductListComponent);
+    return ProductListComponent;
+}());
+export { ProductListComponent };
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/ 
+//# sourceMappingURL=product-list.component.js.map
